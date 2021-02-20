05d003f7bd3e9ae0d5a5f53506d6ae391e7e7891
Donnahue George <Donnahuegjr@gmail.com>
2021-02-20 17:24:51 -0500

    converted files to handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,5 @@
 // Requiring path to so we can use relative routes to our HTML files
-const path = require("path");
+// const path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -10,7 +10,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../views/layouts/signup.html")); //change path to signup handlebar page
+    res.render("signup"); //change path to signup handlebar page
   });
 
   app.get("/login", (req, res) => {
@@ -18,12 +18,12 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("login");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.render("members");
   });
 };
