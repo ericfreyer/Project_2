e4422b352396da17510ae8c90c2925f69bf84373
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-02-24 13:01:14 -0500

    commiting to edit

--- routes/chat-api-routes.js
+++ routes/chat-api-routes.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-const db = require("../models");
-// const passport = require("../config/passport");
-module.exports = app => {
-  app.get("/api/chat/:id", (req, res) => {
-    console.log(req.User);
-    db.User.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then(result => {
-      res.json(result);
-      console.log(result.email);
-    });
-  });
-};
-=======
 // const db = require("../models");
 // // const passport = require("../config/passport");
 // module.exports = app => {
@@ -30,4 +13,3 @@ module.exports = app => {
 //     });
 //   });
 // };
->>>>>>> b87903fb024d023a29d2eaa1b44b5550c2b16022
