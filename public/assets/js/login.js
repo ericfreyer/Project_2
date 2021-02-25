09d392edd6eecc9ba2a6d0f10cd25bc9d512ebf0
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-02-25 10:28:55 -0500

    can now sign in with username instead of email

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,31 +1,32 @@
 $(document).ready(() => {
   // Getting references to our form and inputs
   const loginForm = $("form.login");
-  const emailInput = $("input#email-input");
+  // const emailInput = $("input#email-input");
   const passwordInput = $("input#password-input");
+  const usernameInput = $("input#username-input");
 
   // When the form is submitted, we validate there's an email and password entered
   loginForm.on("submit", event => {
     event.preventDefault();
     const userData = {
-      email: emailInput.val().trim(),
+      username: usernameInput.val().trim(),
       password: passwordInput.val().trim()
     };
 
-    if (!userData.email || !userData.password) {
+    if (!userData.username || !userData.password) {
       return;
     }
 
     // If we have an email and password we run the loginUser function and clear the form
-    loginUser(userData.email, userData.password);
-    emailInput.val("");
+    loginUser(userData.username, userData.password);
+    usernameInput.val("");
     passwordInput.val("");
   });
 
   // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-  function loginUser(email, password) {
+  function loginUser(username, password) {
     $.post("/api/login", {
-      email: email,
+      username: username,
       password: password
     })
       .then(() => {
