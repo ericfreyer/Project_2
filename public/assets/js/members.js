1c89eedde24d98b1f5d0e2758bfe184e89281ed5
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-03-01 19:36:51 -0500

    fixed username to display on page again

--- public/assets/js/members.js
+++ public/assets/js/members.js
@@ -4,4 +4,25 @@ $(document).ready(() => {
   $.get("/api/user_data").then(data => {
     $("#profileName").text(data.username);
   });
+
+//   $('#yourDivName').prepend('yourtHTML');
+
+// <form id="cms">
+// 					<div class="form-group">
+// 						<label for="title">Title:</label>
+// 						<input placeholder="Post Title" type="text" class="form-control" id="title">
+// 						<br />
+// 						<label for="body">Body:</label>
+// 						<textarea placeholder="Post Body" class="form-control" rows="10" id="body"></textarea>
+// 						<div class="form-group">
+// 							<label for="category">Select Author:</label>
+// 							<select class="custom-select" id="author">
+//   						</select>
+// 						</div>
+// 						<br />
+// 						<button type="submit" class="btn btn-success submit">Submit</button>
+// 					</div>
+// 				</form>
+
+
 });
